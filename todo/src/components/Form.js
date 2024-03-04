137413e9bae472c90b967a43e72cf7ecc64c4098
Fakhra99 <fakhrahnajeeb61@gmail.com>
2024-03-05 00:08:23 +0500

    completed todo app project

--- todo/src/components/Form.js
+++ todo/src/components/Form.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { useEffect } from 'react';
 import {v4 as uuidv4} from 'uuid';
 
-const form = ({input, setInput, todos, setTodos}) => {
+const Form = ({input, setInput, todos, setTodos, editTodo, setEditTodo}) => {
+
+   const updateTodo = (title, id, completed) => {
+    const newTodos = todos.map((todo) =>
+        todo.id === id ? { ...todo, title, completed } : todo
+    );
+    setTodos(newTodos);
+    setEditTodo("");
+};
+
+useEffect(() =>{
+  if(editTodo){
+    setInput(editTodo.title);
+  } else {
+    setInput("")
+  }
+}, [setInput, editTodo])
     const onInputChange = (event)=>{
         setInput(event.target.value);
     };
 
     const onFormSubmit =(event) =>{
         event.preventDefault()
-        setTodos([...todos, {id:uuidv4(), title: input, completed: false}]);
+        if(!editTodo){
+           setTodos([...todos, {id:uuidv4(), title: input, completed: false}]);
         setInput("")
+        }else{
+          updateTodo(input, editTodo.id, editTodo.completed)
+        }
     }
   return (
 
     <form onSubmit={onFormSubmit}>
         <input type='text' placeholder='Enter a todo' className='task-input' value={input} required onChange={onInputChange}/>
-        <button className='button-add' type='submit'>Add</button>
+        <button className='button-add' type='submit'>
+          {editTodo ? "OK" : "Add"}
+        </button>
     </form>
   )
 }
 
-export default form
+export default Form;
